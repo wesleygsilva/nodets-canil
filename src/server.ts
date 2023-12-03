2efd55ccd3973363e312ce5ca05d38581ac8d957
Wesley Gomes <gomes.wesley@outlook.com.br>
2023-12-03 13:40:12 -0300

    base criada

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mustache from 'mustache-express'
+import path from 'path'
+import mainRoutes from './routes/index'
+
+dotenv.config()
+
+const server = express()
+
+server.set('view engine', 'mustache')
+server.set('views', path.join(__dirname, 'views'))
+server.engine('mustache', mustache())
+
+server.use(mainRoutes)
+
+server.use((req, res) => {
+    res.send('Página não encontrada!')
+})  
+
+
+server.use(express.static(path.join(__dirname, '../public')))
+
+// Rotas
+
+server.listen(process.env.PORT)
